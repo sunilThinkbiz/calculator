a85f05ad79ab625bb9b083afec31979151e76e6d
unknown <sunilpawar@vebuin.com>
2023-12-04 00:18:51 +0530

    added investment calculation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+
+import { useState } from "react";
+import Header from "./components/Header";
+import { UserInput } from "./components/UserInput";
+import Results from "./components/Results";
+
 
 function App() {
+  const [userInput, setInput] = useState({
+    initialInvestment :1200,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:10
+});
+const inputIsValid = userInput.duration -1
+function handleChange(inputIdentifier, newValue) {
+    setInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        };
+    })
+}
+console.log('fsd',userInput)
+
   return (
-    <div className="App">
-     <h1>hi</h1>
-    </div>
+   <>
+   <Header />
+   <UserInput userInput={userInput} onChangeInput={handleChange} />
+   {!inputIsValid && <p className="center">please enter a duration greater than zero</p>}
+   {inputIsValid && <Results input={userInput} />}
+   </>
   );
 }
 
