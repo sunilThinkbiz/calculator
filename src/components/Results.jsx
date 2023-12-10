7f104c0aa52f29f25f10b943f5548c4135652286
unknown <sunilpawar@vebuin.com>
2023-12-10 22:41:37 +0530

    fix the bug undefined value

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,36 +1,47 @@
-import { calculateInvestmentResults, formatter } from "../util/investment"
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
+
 
 export default function Results({ input }) {
-    const resultsData=[];
-     calculateInvestmentResults(input)
-    if(resultsData.length===0){
-        return <p>data not found</p>
-    }
-    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment
-    console.log(resultsData[0].annualInvestment)
-    return <table id="result">
-        <thead id="result-thead" >
-            <tr>
-                <th>Year</th>
-                <th>Investment Value</th>
-                <th>Invest (year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
+  const results = [];
+  calculateInvestmentResults(input, results);
+  if(results.length===0){
+    return <p className='center'>data not found</p>
+  }
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
-            </thead>
-            <tbody id="result-tbody">
-                {resultsData.map(yearData => {
-                    const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment
-                    const totalAmountInvested = yearData.valueEndOfYear -totalInterest
-                    return (<tr key={yearData.year}>
-                        <td>{yearData.year}</td>
-                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
-                        <td>{formatter.format(yearData.interest)}</td>
-                        <td>{formatter.format(totalInterest)}</td>
-                        <td>{formatter.format(totalAmountInvested)}</td>
-                    </tr>);
-                })}
-            </tbody>
-       
+          );
+        })}
+      </tbody>
     </table>
+  );
 }
